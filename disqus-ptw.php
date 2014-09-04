9a740e5838a75297a3746d160ebc196471fc8c06
hardeepasrani <hardeepasrani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 19:26:33 +0000

    Just formatted the PHP code.
    git-svn-id: https://plugins.svn.wordpress.org/disqus-popular-threads-widget-by-tricks-panda/trunk@980262 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disqus-ptw.php
+++ disqus-ptw.php
@@ -7,105 +7,106 @@ Version: 1.3
 Author: Hardeep Asrani
 Author URI: http://www.hardeepasrani.com
 */
-
 class tp_disquspopularthreads extends WP_Widget
+
 {
-    function tp_disquspopularthreads()
-    {
-        $widget_ops = array(
-            'classname' => 'tp_disquspopularthreads',
-            'description' => 'Add Disqus popular threads widget to WordPress sidebar.'
-        );
-        
-        $this->WP_Widget('tp_disquspopularthreads', 'Disqus Popular Threads Widget', $widget_ops);
-    }
-    
-    function form($instance)
-    {
-        $instance = wp_parse_args((array) $instance);
-        
-        if(!isset($instance['threadnumbers']) || empty($instance['threadnumbers'])) { 
-            $instance['threadnumbers'] = "5"; 
-        }
+	function tp_disquspopularthreads()
+	{
+		$widget_ops = array(
+			'classname' => 'tp_disquspopularthreads',
+			'description' => 'Add Disqus popular threads widget to WordPress sidebar.'
+		);
+		$this->WP_Widget('tp_disquspopularthreads', 'Disqus Popular Threads Widget', $widget_ops);
+	}
+
+	function form($instance)
+	{
+		$instance = wp_parse_args((array)$instance);
+		if (!isset($instance['threadnumbers']) || empty($instance['threadnumbers'])) {
+			$instance['threadnumbers'] = "5";
+		}
+
 ?>
 
 <p>
 <label for="<?php
-        echo $this->get_field_id('title');
+		echo $this->get_field_id('title');
 ?>">
 Title:
 <br/>
 <input id="<?php
-        echo $this->get_field_id('title');
+		echo $this->get_field_id('title');
 ?>" 
 name="<?php
-        echo $this->get_field_name('title');
+		echo $this->get_field_name('title');
 ?>" type="text" value="<?php
-        if(isset($instance['title'])){ echo $instance['title']; }
+		if (isset($instance['title'])) {
+			echo $instance['title'];
+		}
+
 ?>" />
 </label>
 <br/>
 <label for="<?php
-        echo $this->get_field_id('siteid');
+		echo $this->get_field_id('siteid');
 ?>">
 Disqus Site ID:
 <br/>
 <input id="<?php
-        echo $this->get_field_id('siteid');
+		echo $this->get_field_id('siteid');
 ?>" 
 name="<?php
-        echo $this->get_field_name('siteid');
+		echo $this->get_field_name('siteid');
 ?>" type="text" value="<?php
-        if(isset($instance['siteid'])){ echo $instance['siteid']; }
+		if (isset($instance['siteid'])) {
+			echo $instance['siteid'];
+		}
+
 ?>" />
 </label>
 <br/>
 <label for="<?php
-        echo $this->get_field_id('threadnumbers');
+		echo $this->get_field_id('threadnumbers');
 ?>">
 Number of Threads:
 <br/>
 <input id="<?php
-        echo $this->get_field_id('threadnumbers');
+		echo $this->get_field_id('threadnumbers');
 ?>" 
 name="<?php
-        echo $this->get_field_name('threadnumbers');
+		echo $this->get_field_name('threadnumbers');
 ?>" type="number" value="<?php
-        echo $instance['threadnumbers'];
+		echo $instance['threadnumbers'];
 ?>" />
 </label>
 </p>
 
 <?php
-    }
-    function update($new_instance, $old_instance)
-    {
-        $instance                     = $old_instance;
-        $instance['title']           = $new_instance['title'];
-        $instance['siteid']           = $new_instance['siteid'];
-        $instance['threadnumbers']   = $new_instance['threadnumbers'];
-        
-        return $instance;
-    }
-    
-    function widget($args, $instance) // widget sidebar output
-    {
-        extract($args, EXTR_SKIP);
-        
-        echo $before_widget;
-        echo $before_title;
-        echo $instance['title'];
-        echo $after_title;
-        
-        $title           = $instance['title'];
-        $siteid           = $instance['siteid'];
-        $threadnumbers   = $instance['threadnumbers'];
-        
-        echo "<div id='popularthreads' class='dsq-widget'><script type='text/javascript' src='http://$siteid.disqus.com/popular_threads_widget.js?num_items=$threadnumbers'></script></div>";
-        echo $after_widget;
-    }
+	}
+
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		$instance['siteid'] = $new_instance['siteid'];
+		$instance['threadnumbers'] = $new_instance['threadnumbers'];
+		return $instance;
+	}
+
+	function widget($args, $instance) // widget sidebar output
+	{
+		extract($args, EXTR_SKIP);
+		echo $before_widget;
+		echo $before_title;
+		echo $instance['title'];
+		echo $after_title;
+		$title = $instance['title'];
+		$siteid = $instance['siteid'];
+		$threadnumbers = $instance['threadnumbers'];
+		echo "<div id='popularthreads' class='dsq-widget'><script type='text/javascript' src='http://$siteid.disqus.com/popular_threads_widget.js?num_items=$threadnumbers'></script></div>";
+		echo $after_widget;
+	}
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("tp_disquspopularthreads");'));
-
 ?>
